560d9824b85af9c252cd8214697642e77b679cf5
Haiyang Hong <hong961127@gmail.com>
2020-10-14 12:52:16 -0700

    Add banner section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 import "./styles/App.scss";
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
   );
 }
